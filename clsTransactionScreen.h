eba52fe22a8c6fffa210c28a59c7bbdcf2f9ed3c
Yousef Nasser <yousef.work.18@gmail.com>
2026-02-16 14:12:48 +0200

    Add clsTransactionScreen Class
    Implemented the Transactions Sub-menu.
    
    Created a dedicated screen for financial operations.
    
    Added menu options for Deposit, Withdraw, and Total Balances.
    
    Implemented navigation logic to return to the Main Menu.
    
    Acts as the controller for all transaction-related screens.

---
+++ clsTransactionScreen.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "clsScreen.h"
+#include "clsInputValidate.h"
+#include "clsDepositScreen.h"
+#include "clsWithdrawScreen.h"
+class clsTransactionScreen:protected clsScreen
+{
+private:
+	enum enTransactionsMenu {
+		deposit = 1, withdraw = 2, totalBalances = 3, returnToMainMenu = 4
+	};
+	static enTransactionsMenu _NumberToTransactionMenuChoicesEnum(int choice) {
+		return static_cast<enTransactionsMenu>(choice);
+	}
+	static void _ReturnToTransactionScreen() {
+		system("pause");
+		system("cls");
+	}
+	static void _TransactionsMenuScreen() {
+		clsScreen::DrawScreen("Transactions");
+		std::cout << "\t\t\t\t\t      " << "[1] Deposit" << std::endl;
+		std::cout << "\t\t\t\t\t      " << "[2] Withdraw" << std::endl;
+		std::cout << "\t\t\t\t\t      " << "[3] Show Total Balance" << std::endl;
+		std::cout << "\t\t\t\t\t      " << "[4] Return to main menu" << std::endl;
+		std::cout << "\t\t\t\t    " << "___________________________________________\n" << std::endl;
+	}
+	static int _GetTransactionChoice() {
+		std::cout << "\t\t\t\t    Choose A Number Between[1-4]:";
+		int number = clsInputValidate::ReadNumberBetween(1, 4, "Please Enter a Number Between[1-4]:");
+		return number;
+	}
+	static void _ShowDepositOption() {
+		clsDepositScreen::ShowDepositOption();
+		_ReturnToTransactionScreen();
+	}
+	static void _ShowWithDrawScreen() {
+		clsWithdrawScreen::ShowWithdrawOption();
+		_ReturnToTransactionScreen();
+	}
+	static enTransactionsMenu _PerformTransactionOptions() {
+		enTransactionsMenu choice = _NumberToTransactionMenuChoicesEnum(_GetTransactionChoice());
+		switch (choice) {
+
+		case enTransactionsMenu::deposit:
+			_ShowDepositOption();
+			break;
+		case enTransactionsMenu::withdraw:
+			_ShowWithDrawScreen();
+
+
+			break;
+		case enTransactionsMenu::totalBalances:
+			std::cout << "Coming Soon" << std::endl;
+			break;
+		}
+		return choice;
+	}
+public:
+	
+	static void TransactionScreen() {
+		do {
+			_TransactionsMenuScreen();
+		} while (_PerformTransactionOptions() != enTransactionsMenu::returnToMainMenu);
+	}
+
+};
+
