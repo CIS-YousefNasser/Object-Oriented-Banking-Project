f1262bada4fa24f8312cdd67b845318c96f2e7f0
Yousef Nasser <yousef.work.18@gmail.com>
2026-02-17 21:31:54 +0200

    Implement Dynamic Permission-Based Main Menu
    Overhauled Main Menu to enforce Role-Based Access Control (RBAC).
    
    Dynamic Rendering: Menu options are now conditionally displayed based on currentUser.IsAllowedAccess().
    
    Menu Re-indexing: Implemented vOptions vector to dynamically map user inputs (1, 2, 3...) to the correct internal system enums, ensuring users see a contiguous list of options regardless of their permissions.
    
    Integrated all sub-systems: Clients, Transactions, and User Management.

--- clsMainMenuScreen.h
+++ clsMainMenuScreen.h
@@ -9,13 +9,17 @@
 #include "clsFindClients.h"
 #include "clsTransactionScreen.h"
 #include "clsManageUserScreen.h"
+#include "Global.h"
 class clsMainMenuScreen:protected clsScreen
 {
 private:
-	enum enMainMenuOptions {
-		showClientList=1,addNewClient=2,deleteClient=3,updateClientInfo=4,findClient=5,Transactions=6,manageUsers=7,logout=8
+	enum enAllowAccess {
+		all = -1, showOptionCode = 1, addOptionCode = 2, deleteOptionCode = 4, searchOptionCode = 8, updateOptionCode = 16, transactionsOptionCode = 32, usersMenuCode = 64,logout=128
+
 	};
-	
+	static short enAllowAccessToShort(enAllowAccess choice) {
+		return static_cast<short>(choice);
+	}
 	static void ReturnToMainMenu() {
 		system("pause");
 		system("cls");
@@ -48,42 +52,41 @@ private:
 		clsManageUserScreen::ShowUserScreen();
 		ReturnToMainMenu();
 	}
-	static enMainMenuOptions _NumberToMainMenuOptions(int number) {
-		return static_cast<enMainMenuOptions>(number);
-	}
-	static int _GetMainMenuChoice() {
-		std::cout << "\t\t\t\t    Choose A Number Between[1-8]:";
-		int number=clsInputValidate::ReadNumberBetween(1,8,"Please Enter a Number Between[1-8]:");
+	static short _GetMainMenuChoice(short &to) {
+		std::cout << "\t\t\t\t    Choose A Number Between[1-"<<to<<"]:";
+		short number=clsInputValidate::ReadNumberBetween(1,to,"Please Enter a Number Between[1-"+std::to_string(to)+"]:");
 		return number;
 	}
+	static enAllowAccess _NumberToEnumAllowAccess(short choice, std::vector<short>& vOptions) {
+		return static_cast<enAllowAccess>(vOptions[choice-1]);
+	}
 	
-	static enMainMenuOptions _PerformMainMenuOptions() {
-		enMainMenuOptions choice = _NumberToMainMenuOptions(_GetMainMenuChoice());
+	static enAllowAccess _PerformMainMenuOptions(short &to, std::vector<short> &vOptions) {
+		enAllowAccess choice = _NumberToEnumAllowAccess(_GetMainMenuChoice(to),vOptions);
 		system("cls");
 		switch (choice) {
-		case enMainMenuOptions::showClientList:
+		case enAllowAccess::showOptionCode:
 			_ShowClientListScreen();
 			break;
-		case enMainMenuOptions::addNewClient:
+		case enAllowAccess::addOptionCode:
 			_AddClientsScreen();
 			break;
-		case enMainMenuOptions::deleteClient:
+		case enAllowAccess::deleteOptionCode:
 			_DeleteClientsScreen();
 			break;
-		case enMainMenuOptions::updateClientInfo:
+		case enAllowAccess::updateOptionCode:
 			_UpdateClientInfoScreen();
 			break;
-		case enMainMenuOptions::findClient:
+		case enAllowAccess::searchOptionCode:
 			_FindClientsScreen();
 			break;
-		case enMainMenuOptions::Transactions:
+		case enAllowAccess::transactionsOptionCode:
 			_TransactionsScreen();
 			break;
-		case enMainMenuOptions::manageUsers:
+		case enAllowAccess::usersMenuCode:
 			_ManageUsersScreen();
 			break;
-		case enMainMenuOptions::logout:
-			
+		case enAllowAccess::logout:
 			break;
 		default:
 			std::cout << "Error In Perform Main Menu Options" << std::endl;
@@ -91,25 +94,82 @@ private:
 		return choice;
 
 	}
+	static void Logout() {
+		currentUser.GetEmptyUser();
+	}
 
 public:
 	static void MainMenuScreen() {
+		std::vector<short> vOptions;
+		bool collectedOptions=false;
+		short numberOfMenu;
 		do {
+			numberOfMenu = 0;
 			clsScreen::DrawScreen("Main Menu");
-			std::cout << "\t\t\t\t    ___________________________________________\n" << std::endl;
-			std::cout << "\t\t\t\t\t\t " << "Main Menu Options" << std::endl;
-			std::cout << "\t\t\t\t    " << "___________________________________________\n" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[1] Show Client List" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[2] Add New Client" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[3] Delete Client" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[4] Update Client Info" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[5] Find Client" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[6] Transactions" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[7] Manage Users" << std::endl;
-			std::cout << "\t\t\t\t\t      " << "[8] Logout" << std::endl;
-			std::cout << "\t\t\t\t    " << "___________________________________________\n" << std::endl;
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::showOptionCode))) {
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Show Client List" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::showOptionCode));
+				}
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::addOptionCode))) {
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Add New Client" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::addOptionCode));
+				}
+				
 
-		} while (_PerformMainMenuOptions() != enMainMenuOptions::logout);
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::deleteOptionCode))) {
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Delete Client" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::deleteOptionCode));
+				}
+				
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::updateOptionCode))){
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Update Client Info" << std::endl;
+				if (!collectedOptions) {
+                 vOptions.push_back(enAllowAccessToShort(enAllowAccess::updateOptionCode));
+				}
+				
+
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::searchOptionCode))){
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Find Client" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::searchOptionCode));
+				}
+				
+
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::transactionsOptionCode))){
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Transactions" << std::endl;
+				if (!collectedOptions) {
+                 vOptions.push_back(enAllowAccessToShort(enAllowAccess::transactionsOptionCode));
+				}
+				
+
+			}
+			if (currentUser.IsAllowedAccess(enAllowAccessToShort(enAllowAccess::usersMenuCode))){
+				std::cout << "\t\t\t\t\t      " << "[" << ++numberOfMenu << "] Manage Users" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::usersMenuCode));
+				}
+				
+			}
+			
+				std::cout << "\t\t\t\t\t      " << "[" <<++numberOfMenu << "] Logout" << std::endl;
+				if (!collectedOptions) {
+					vOptions.push_back(enAllowAccessToShort(enAllowAccess::logout));
+
+			}
+			
+			std::cout << "\t\t\t\t    " << "___________________________________________\n" << std::endl;
+			collectedOptions = true;
+		} while (_PerformMainMenuOptions(numberOfMenu,vOptions) != enAllowAccess::logout);
+
+		Logout();
 	}
 };
 
